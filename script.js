095e363f2086e0723fc6063fc48cd781581d42a7
freitinhassss <22300732@aluno.cotemig.com.br>
2024-11-01 23:40:16 -0300

    commit final2

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 function toggleDarkMode() {
-  document.body.classList.toggle("dark-mode");
+  document.body.classList.toggle('dark-mode')
 }
+
+const greetings = ["Bem-vindo!", "Prazer em conhecer você!", "Olá!"];
+let index = 0;
+
+function changeGreeting() {
+    const greetingElement = document.querySelector(".prazer");
+    greetingElement.textContent = greetings[index];
+    index = (index + 1) % greetings.length;
+}
+
+setInterval(changeGreeting, 3000);
+
+function msg() {
+  alert("Você está prestes a ver o portfólio de Henrique Freitas!");
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      window.scrollTo({
+          top: targetElement.offsetTop,
+          behavior: "smooth"
+      });
+  });
+});
